4a6f8623f17eb5eb88943ca9701d148ac28365bf
Andreas Rudolf <andreas.rudolf@hs-augsburg.de>
2019-05-24 20:49:45 +0200

    Merge branch 'master' into 'features/req8-calculator'
    # Conflicts:
    #   .gitignore
    #   index.js

--- index.js
+++ index.js
@@ -8,19 +8,26 @@ import CalculatorOnOffEventListener from "./src/main/listener/CalculatorOnOffEve
 import AlertWindow from "./src/main/gui/AlertWindow.js";
 import NavigationEventListener from "./src/main/listener/NavigationEventListener.js"
 import CalculationLogWindow from "./src/main/gui/CalculationLogWindow.js";
+import Calculator from "./src/main/Business/Calculator.js.js";
+import Term from "./src/main/data/Term.js";
+import TermBuilder from "./src/main/Business/TermBuilder.js.js";
 
     //TODO: add subscribers to the listener
     //TODO: add the keyboard listener
 
 // Start calculator setup
-let calculatorWindow = new CalculatorWindow();
-let alertWindow = new AlertWindow();
-let calculationLogWindow = new CalculationLogWindow();
-let calculatorOnOffEventListener = new CalculatorOnOffEventListener(calculatorWindow, alertWindow);
-let calculationEventListener = new CalculationEventListener();
-let keyboardEventListener = new KeyboardEventListener(calculationEventListener, calculatorOnOffEventListener, alertWindow);
-let navigationEventListener = new NavigationEventListener(calculationLogWindow);
-let calculatorSetup = new CalculatorSetup();
+const calculatorWindow = new CalculatorWindow();
+const alertWindow = new AlertWindow();
+const calculationLogWindow = new CalculationLogWindow();
+const calculatorOnOffEventListener = new CalculatorOnOffEventListener(calculatorWindow, alertWindow);
+const calculationEventListener = new CalculationEventListener();
+const keyboardEventListener = new KeyboardEventListener(calculationEventListener, calculatorOnOffEventListener, alertWindow);
+const navigationEventListener = new NavigationEventListener(calculationLogWindow);
+const calculatorSetup = new CalculatorSetup();
+const calculator = new Calculator();
+const term = new Term();
+const termBuilder = new TermBuilder(term, calculator, alertWindow);
+calculationEventListener.addSubscriber(termBuilder);
 
 // This code will run after the window loading process finished
 // Otherwise it wouldn´t be possible to initialize the eventListeners, as the html-elements havent been created till then.
@@ -32,7 +39,6 @@ window.onload = () => {
     calculatorSetup.setupAlertWindows(alertWindow);
     calculatorSetup.setupCalculatorWindow(calculatorWindow);
     calculatorSetup.setupNavigationEventListener(navigationEventListener);
-
 
     console.log("Calculator Set-Up completed");
 }
---
+++ src/main/Business/Calculator.js
@@ -0,0 +1,34 @@
+'use strict';
+import Term from "../data/Term.js"
+
+export default class Calculator {
+    /**
+     * Constructor which initializes the needed term and an array so save the last entries
+     * 
+     * 
+     */
+    constructor() {
+        // build log of last 5 terms
+        this.__log = [];
+    }
+
+    /**
+     * Calculate result of term depending on operator
+     * Push the calculated term to log
+     */
+    calculate (term) {       
+        if (term.operator === 'add') {
+            term.result = (parseFloat(term.num1) + parseFloat(term.num2)).toString();
+        } else if (term.operator === 'multiply') {
+            term.result = (parseFloat(term.num1) * parseFloat(term.num2)).toString();
+        } else if (term.operator === 'subtract') {
+            term.result = (parseFloat(term.num1) - parseFloat(term.num2)).toString();
+        } else if (term.operator === 'divide') {
+            term.result = (parseFloat(term.num1) / parseFloat(term.num2)).toString();
+        }
+        term.ans = term.result;
+        // push to log to save the last 5 logs
+        // build intelligence to containt only last 5
+        this.__log.push(term);
+    }
+}
