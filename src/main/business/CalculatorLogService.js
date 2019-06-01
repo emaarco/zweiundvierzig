2d6b9c5ffd96d61b91abf3b4fc3cb92048b0d0e2
emaarco <marco.schaeck@outlook.de>
2019-06-01 03:37:45 +0200

    req10/11: resolve typo

--- src/main/business/CalculatorLogService.js
+++ src/main/business/CalculatorLogService.js
@@ -109,6 +109,7 @@ export default class CalculatorLogService {
     loadLogFromServer(logKey, callback) {
 
         let exception = null;
+        let loadedLog = [];
 
         try {
             setTimeout(() => {
